1b6cd7aca443bc6fa6fa7bdfb9db46cc774c1d3d
Chanin Nantasenamat <51851491+dataprofessor@users.noreply.github.com>
2022-11-04 22:37:21 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,9 +1,13 @@
 import streamlit as st
-from stmol import showmol, render_pdb
+from stmol import showmol, render_pdb, obj_upload
 import py3Dmol
 
 st.set_page_config(layout = 'wide')
 
 st.title('🎈 ESMfold')
 
-showmol(render_pdb(id = '3EQM'))
+uploaded_file = st.sidebar.file_uploader('Upload PDB file')
+
+#showmol(render_pdb(id = '3EQM'))
+
+showmol(obj_upload(uploaded_file))
