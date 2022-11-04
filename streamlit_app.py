38373fbe657a8a6c31532c46044914741d53313d
Chanin Nantasenamat <51851491+dataprofessor@users.noreply.github.com>
2022-11-04 22:33:06 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,6 +2,8 @@ import streamlit as st
 from stmol import showmol, render_pdb
 import py3Dmol
 
+st.set_page_config(layout = 'wide')
+
 st.title('🎈 ESMfold')
 
-showmol(render_pdb(id = '1A2C'))
+showmol(render_pdb(id = '3EQM'))
