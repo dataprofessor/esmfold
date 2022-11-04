d57cccdeb7dc1fd03b54bf4000cc5f29b906ecc2
Chanin Nantasenamat <51851491+dataprofessor@users.noreply.github.com>
2022-11-04 22:50:51 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,15 @@
 import streamlit as st
 from stmol import showmol, render_pdb, obj_upload
 import py3Dmol
+from transformers import AutoTokenizer, EsmForProteinFolding
 
+# ESMfold
+tokenizer = AutoTokenizer.from_pretrained("facebook/esmfold_v1")
+model = EsmForProteinFolding.from_pretrained("facebook/esmfold_v1", low_cpu_mem_usage=True)
+
+model = model.cuda()
+
+# The App
 st.set_page_config(layout = 'wide')
 
 st.title('🎈 ESMfold')
