e82269afebdd8c3fc458aea69a1b5510c0e26719
Chanin Nantasenamat <51851491+dataprofessor@users.noreply.github.com>
2022-11-05 15:18:51 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,52 +1,13 @@
 import streamlit as st
 from stmol import showmol, render_pdb, obj_upload
 import py3Dmol
-from transformers import AutoTokenizer, EsmForProteinFolding
-from transformers.models.esm.openfold_utils.protein import to_pdb, Protein as OFProtein
-from transformers.models.esm.openfold_utils.feats import atom14_to_atom37
-import torch
 
 # ESMfold
-tokenizer = AutoTokenizer.from_pretrained("facebook/esmfold_v1")
-model = EsmForProteinFolding.from_pretrained("facebook/esmfold_v1", low_cpu_mem_usage=True)
-
-test_protein = "MGA"
-tokenized_input = tokenizer([test_protein], return_tensors="pt", add_special_tokens=False)['input_ids']
-
-with torch.no_grad():
-    output = model(tokenized_input)
-
-def convert_outputs_to_pdb(outputs):
-    final_atom_positions = atom14_to_atom37(outputs["positions"][-1], outputs)
-    outputs = {k: v.to("cpu").numpy() for k, v in outputs.items()}
-    final_atom_positions = final_atom_positions.cpu().numpy()
-    final_atom_mask = outputs["atom37_atom_exists"]
-    pdbs = []
-    for i in range(outputs["aatype"].shape[0]):
-        aa = outputs["aatype"][i]
-        pred_pos = final_atom_positions[i]
-        mask = final_atom_mask[i]
-        resid = outputs["residue_index"][i] + 1
-        pred = OFProtein(
-            aatype=aa,
-            atom_positions=pred_pos,
-            atom_mask=mask,
-            residue_index=resid,
-            b_factors=outputs["plddt"][i],
-            chain_index=outputs["chain_index"][i] if "chain_index" in outputs else None,
-        )
-        pdbs.append(to_pdb(pred))
-    return pdbs
-
-pdb = convert_outputs_to_pdb(output)
 
 # The App
 st.set_page_config(layout = 'wide')
 
 st.title('🎈 ESMfold')
-
-
-st.write(pdb)
 
 
 #uploaded_file = st.sidebar.file_uploader('Upload PDB file')
