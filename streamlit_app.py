69992427eeefbf6636a31b6af3310971ee2244ea
Chanin Nantasenamat <51851491+dataprofessor@users.noreply.github.com>
2022-11-05 16:51:53 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,27 +4,3 @@ import py3Dmol
 import requests
 import tempfile
 
-# ESMfold
-
-DEFAULT_SEQ = "MGSSHHHHHHSSGLVPRGSHMRGPNPTAASLEASAGPFTVRSFTVSRPSGYGAGTVYYPTNAGGTVGAIAIVPGYTARQSSIKWWGPRLASHGFVVITIDTNSTLDQPSSRSSQQMAALRQVASLNGTSSSPIYGKVDTARMGVMGWSMGGGGSLISAANNPSLKAAAPQAPWDSSTNFSSVTVPTLIFACENDSIAPVNSSALPIYDSMSRNAKQFLEINGGSHSCANSGNSNQALIGKKGVAWMKRFMDNDTRYSTFACENPNSTRVSDFRTANCSLEDPAANKARKEAELAAATAEQ"
-
-txt = st.text_area('Input sequence', DEFAULT_SEQ)
-
-
-st.write(txt)
-#st.button('Predict', on_click=update)
-
-# The App
-st.set_page_config(layout = 'wide')
-
-st.title('🎈 ESMfold')
-
-
-#uploaded_file = st.sidebar.file_uploader('Upload PDB file')
-
-##showmol(render_pdb(id = '3EQM'))
-
-#if uploaded_file is not None:
-#  showmol(obj_upload(uploaded_file).setStyle({'cartoon':{'color':'spectrum'}}) )
-#else:
-#  st.info('Awaiting input ...')
