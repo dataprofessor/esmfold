e4724c136f50a1bdb56e75252b52774659b86c65
Chanin Nantasenamat <51851491+dataprofessor@users.noreply.github.com>
2022-11-05 21:23:01 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -24,11 +24,12 @@ def update(sequence=txt):
     name = sequence[:3] + sequence[-3:] 
     pdb_string = response.content.decode('utf-8')
     
-    tmp = tempfile.NamedTemporaryFile()
+    #tmp = tempfile.NamedTemporaryFile()
     #with open(tmp.name, "w") as f:
     #    f.write(pdb_string)
     #st.write("File name", tmp.name)
-    return st.write(pdb_string)
+    obj = makeobj(pdb_string,'pdb',style='cartoon')
+    return showmol(obj)
 
 st.button('Predict', on_click=update)
 
