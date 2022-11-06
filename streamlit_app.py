479e79b6ef5236a2fe2f75230c9c82a3022614d2
Chanin Nantasenamat <51851491+dataprofessor@users.noreply.github.com>
2022-11-07 00:34:13 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -9,7 +9,7 @@ import biotite.structure.io as bsio
 
 #st.set_page_config(layout = 'wide')
 st.sidebar.title('🎈 ESMFold')
-st.sidebar.write('[*ESMFold*](https://esmatlas.com/about) is an end-to-end single sequence protein structure predictor based on the ESM-2 language model. More info [here](https://www.biorxiv.org/content/10.1101/2022.07.20.500902v2)')
+st.sidebar.write('[*ESMFold*](https://esmatlas.com/about) is an end-to-end single sequence protein structure predictor based on the ESM-2 language model. For more information, read the [research article](https://www.biorxiv.org/content/10.1101/2022.07.20.500902v2) and the [news article](https://www.nature.com/articles/d41586-022-03539-1) published in *Nature*.')
 
 # stmol
 def render_mol(pdb):
