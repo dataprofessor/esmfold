b9d7a9f350f159c1982fd2cfd927aa2038f88281
Chanin Nantasenamat <51851491+dataprofessor@users.noreply.github.com>
2022-11-06 22:08:24 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,35 +8,25 @@ import tempfile
 st.title('🎈 ESMfold')
 
 # stmol
-
 def render_mol(pdb):
     pdbview = py3Dmol.view(width=400,height=400)
     pdbview.addModel(pdb,'pdb')
-    pdbview.setStyle({'cartoon':{}})
+    pdbview.setStyle({'cartoon':{'color':'spectrum'}})
     pdbview.setBackgroundColor('white')#('0xeeeeee')
     pdbview.zoomTo()
     showmol(pdbview, height = 500,width=800)
 
 # ESMfold
-
 DEFAULT_SEQ = "MGSSHHHHHHSSGLVPRGSHMRGPNPTAASLEASAGPFTVRSFTVSRPSGYGAGTVYYPTNAGGTVGAIAIVPGYTARQSSIKWWGPRLASHGFVVITIDTNSTLDQPSSRSSQQMAALRQVASLNGTSSSPIYGKVDTARMGVMGWSMGGGGSLISAANNPSLKAAAPQAPWDSSTNFSSVTVPTLIFACENDSIAPVNSSALPIYDSMSRNAKQFLEINGGSHSCANSGNSNQALIGKKGVAWMKRFMDNDTRYSTFACENPNSTRVSDFRTANCSLEDPAANKARKEAELAAATAEQ"
-
 txt = st.text_area('Input sequence', DEFAULT_SEQ)
 
 def update(sequence=txt):
     headers = {
         'Content-Type': 'application/x-www-form-urlencoded',
     }
-
     response = requests.post('https://api.esmatlas.com/foldSequence/v1/pdb/', headers=headers, data=sequence)
     name = sequence[:3] + sequence[-3:] 
     pdb_string = response.content.decode('utf-8')
-    
-    #tmp = tempfile.NamedTemporaryFile()
-    #with open(tmp.name, "w") as f:
-    #    f.write(pdb_string)
-    #st.write("File name", tmp.name)
-    #obj = makeobj(pdb_string, molformat='pdb', style='cartoon')
     return render_mol(pdb_string)
 
 st.button('Predict', on_click=update)
